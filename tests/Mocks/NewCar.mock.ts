9a786ceeab7a3e9495d900be6ac53f1833ddd886
Ricardo_Souza <rl03souza@gmail.com>
2023-02-13 18:21:25 -0300

    Mocks for tests

---
+++ tests/Mocks/NewCar.mock.ts
@@ -0,0 +1,29 @@
+import Car from '../../src/Domains/Car';
+import ICar from '../../src/Interfaces/ICar';
+
+const carInput: ICar = {
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+  
+};
+
+const carOutput: Car = new Car({
+  id: '6348513f34c397abcad040b2',
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.990,
+  doorsQty: 4,
+  seatsQty: 5,
+});
+
+export {
+  carInput,
+  carOutput,
+};
