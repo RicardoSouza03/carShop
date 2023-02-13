8221ffdf6fb20f9fd612549166f94e384f2fd9b5
Ricardo_Souza <rl03souza@gmail.com>
2023-02-13 17:36:53 -0300

    Feat: creating method getById on service with validations

--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -1,9 +1,11 @@
 import Car from '../Domains/Car';
 import ICar from '../Interfaces/ICar';
 import CarODM from '../Models/CarODM';
+import CarValidate from '../Utils/Validations/CarValidations';
 
 export default class CarService {
   private carODM = new CarODM();
+  private carValidations = new CarValidate();
 
   private createCar(car: ICar | null): Car | null {
     if (car) {
@@ -20,5 +22,14 @@ export default class CarService {
   public async getAll() {
     const allCars = await this.carODM.getAll();
     return allCars.map((car) => this.createCar(car));
+  }
+
+  public async getById(id: string) {
+    this.carValidations.validateCarId(id);
+    
+    const foundCar = await this.carODM.getById(id);
+    this.carValidations.checkCarExistence(foundCar);
+
+    return this.createCar(foundCar);
   }
 }
