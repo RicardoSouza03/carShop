fc8fdb1ff25544ac37ce97e44fe5f4a741f417a0
Ricardo_Souza <rl03souza@gmail.com>
2023-02-13 17:02:05 -0300

    Feat: creating method to create object from domain

--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -1,11 +1,19 @@
+import Car from '../Domains/Car';
 import ICar from '../Interfaces/ICar';
 import CarODM from '../Models/CarODM';
 
 export default class CarService {
   private carODM = new CarODM();
 
-  public async registerCar(car: ICar): Promise<ICar> {
+  private createCar(car: ICar | null): Car | null {
+    if (car) {
+      return new Car(car);
+    }
+    return null;
+  }
+
+  public async registerCar(car: ICar) {
     const newCar = await this.carODM.create(car);
-    return newCar;
+    return this.createCar(newCar);
   } 
 }
