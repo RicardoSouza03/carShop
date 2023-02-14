08eb0fce01e0c6643d94a83d42e66279e636f219
Ricardo_Souza <rl03souza@gmail.com>
2023-02-14 14:40:26 -0300

    Refactor: domain car to extend new Vehicle domain

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,43 +1,15 @@
 import ICar from '../Interfaces/ICar';
+import Vehicle from './Vehicle';
 
-export default class Car {
-  protected id: string | undefined;
-  protected model: string;
-  protected year: number;
-  protected color: string;
-  protected status: boolean | undefined;
-  protected buyValue: number;
+export default class Car extends Vehicle {
   private doorsQty: number;
   private seatsQty: number;
 
   constructor(newCar: ICar) {
-    this.id = newCar.id;
-    this.model = newCar.model;
-    this.year = newCar.year;
-    this.color = newCar.color;
-    this.status = newCar.status;
-    this.buyValue = newCar.buyValue;
+    super(newCar);
     this.doorsQty = newCar.doorsQty;
     this.seatsQty = newCar.seatsQty;
   }
-
-  public getId() { return this.id; }
-  public setId(newId: string) { this.id = newId; }
-
-  public getModel() { return this.model; }
-  public setModel(newModel: string) { this.model = newModel; }
-
-  public getYear() { return this.year; }
-  public setYear(newYear: number) { this.year = newYear; }
-
-  public getColor() { return this.color; }
-  public setColor(newColor: string) { this.color = newColor; }
-
-  public getStatus() { return this.status; }
-  public setStatus(newStatus: boolean) { this.status = newStatus; }
-
-  public getBuyValue() { return this.buyValue; }
-  public setBuyValue(newValue: number) { this.buyValue = newValue; }
 
   public getDoorsQty() { return this.doorsQty; }
   public setDoorsQty(newQty: number) { this.doorsQty = newQty; }
