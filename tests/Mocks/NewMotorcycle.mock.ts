9989a4077927f14b643495666ceee973f631e2d4
Ricardo_Souza <rl03souza@gmail.com>
2023-02-14 14:42:53 -0300

    Tests: adding imports to motorcycle mocks

--- tests/Mocks/NewMotorcycle.mock.ts
+++ tests/Mocks/NewMotorcycle.mock.ts
@@ -1,21 +1,25 @@
+import Motorcycle from '../../src/Domains/Motorcycle';
+import IMotorycle from '../../src/Interfaces/IMotorcycle';
+import EnumCategory from '../../src/Utils/EnumCategory';
+
 const motorcycleInput: IMotorycle = {
   model: 'Honda Cb 600f Hornet',
   year: 2005,
   color: 'Yellow',
   status: true,
   buyValue: 30.000,
-  category: 'Street',
+  category: EnumCategory.Street,
   engineCapacity: 600,
 };
 
-const motorcycleOutPut: Motorcyle = new Motorcyle({
+const motorcycleOutPut: Motorcycle = new Motorcycle({
   id: '6348513f34c397abcad040b2',
   model: 'Honda Cb 600f Hornet',
   year: 2005,
   color: 'Yellow',
   status: true,
   buyValue: 30.000,
-  category: 'Street',
+  category: EnumCategory.Street,
   engineCapacity: 600,
 });
 
