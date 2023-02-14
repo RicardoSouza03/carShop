acd0eac2d409f0486e90763696a5e1ce0dbde657
Ricardo_Souza <rl03souza@gmail.com>
2023-02-14 18:01:07 -0300

    Refactor: services to extend from abstract service

--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -1,53 +1,18 @@
 import Car from '../Domains/Car';
 import ICar from '../Interfaces/ICar';
 import CarODM from '../Models/CarODM';
-import CarValidate from '../Utils/Validations/CarValidations';
+import AbstractService from './AbstractService';
 
-export default class CarService {
-  private carODM = new CarODM();
-  private carValidations = new CarValidate();
+export default class CarService extends AbstractService<ICar, Car> {
+  constructor() {
+    const carODM = new CarODM();
+    super(carODM);
+  }
 
-  private createCar(car: ICar | null): Car | null {
+  public createVehicle(car: ICar | null): Car | null {
     if (car) {
       return new Car(car);
     }
     return null;
-  }
-
-  public async registerCar(car: ICar) {
-    const newCar = await this.carODM.create(car);
-    return this.createCar(newCar);
-  }
-
-  public async getAll() {
-    const allCars = await this.carODM.getAll();
-    return allCars.map((car) => this.createCar(car));
-  }
-
-  public async getById(id: string) {
-    this.carValidations.validateCarId(id);
-    
-    const foundCar = await this.carODM.getById(id);
-    this.carValidations.checkCarExistence(foundCar);
-
-    return this.createCar(foundCar);
-  }
-
-  public async updateById(id: string, newInfo: ICar) {
-    this.carValidations.validateCarId(id);
-
-    await this.carODM.updateById(id, newInfo);
-    const updatedCar = await this.getById(id);
-
-    return updatedCar;
-  }
-
-  public async deleteById(id: string): Promise<void> {
-    this.carValidations.validateCarId(id);
-
-    const foundMotorcycle = await this.carODM.getById(id);
-    this.carValidations.checkCarExistence(foundMotorcycle);
-
-    await this.carODM.delete(id);
   }
 }
--- src/Services/MotorcycleService.ts
+++ src/Services/MotorcycleService.ts
@@ -1,53 +1,18 @@
 import Motorcycle from '../Domains/Motorcycle';
 import IMotorycle from '../Interfaces/IMotorcycle';
 import MotorcycleODM from '../Models/Motorcycle';
-import MotorcycleValidate from '../Utils/Validations/MotorcycleValidations';
+import AbstractService from './AbstractService';
 
-export default class MotorcycleService {
-  private motorcycleODM = new MotorcycleODM();
-  private motorcycleValidations = new MotorcycleValidate();
+export default class MotorcycleService extends AbstractService<IMotorycle, Motorcycle> {
+  constructor() {
+    const motorcycleODM = new MotorcycleODM();
+    super(motorcycleODM);
+  }
 
-  private createMotorcycle(motorcycle: IMotorycle | null): Motorcycle | null {
+  createVehicle(motorcycle: IMotorycle | null): Motorcycle | null {
     if (motorcycle) {
       return new Motorcycle(motorcycle);
     }
     return null;
-  }
-
-  public async registerMotorcycle(motorcycle: IMotorycle) {
-    const newMotorcycle = await this.motorcycleODM.create(motorcycle);
-    return this.createMotorcycle(newMotorcycle);
-  }
-
-  public async getAll() {
-    const allMotorcycles = await this.motorcycleODM.getAll();
-    return allMotorcycles.map((motorcycle) => this.createMotorcycle(motorcycle));
-  }
-
-  public async getById(id: string) {
-    this.motorcycleValidations.validateMotorcycleId(id);
-    
-    const foundMotorcycle = await this.motorcycleODM.getById(id);
-    this.motorcycleValidations.checkMotorcycleExistence(foundMotorcycle);
-
-    return this.createMotorcycle(foundMotorcycle);
-  }
-
-  public async updateById(id: string, newInfo: IMotorycle) {
-    this.motorcycleValidations.validateMotorcycleId(id);
-
-    await this.motorcycleODM.updateById(id, newInfo);
-    const updatedMotorcycle = await this.getById(id);
-
-    return updatedMotorcycle;
-  }
-
-  public async deleteById(id: string): Promise<void> {
-    this.motorcycleValidations.validateMotorcycleId(id);
-
-    const foundMotorcycle = await this.motorcycleODM.getById(id);
-    this.motorcycleValidations.checkMotorcycleExistence(foundMotorcycle);
-
-    await this.motorcycleODM.delete(id);
   }
 }
