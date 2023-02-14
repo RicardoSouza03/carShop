07dd77b6e40ddb68c679374a861f69aea3be22aa
Ricardo_Souza <rl03souza@gmail.com>
2023-02-14 15:06:46 -0300

    Fix: domain motorcycle to recive an string on category

--- src/Domains/Motorcycle.ts
+++ src/Domains/Motorcycle.ts
@@ -1,9 +1,8 @@
 import IMotorycle from '../Interfaces/IMotorcycle';
-import EnumCategory from '../Utils/EnumCategory';
 import Vehicle from './Vehicle';
 
 export default class Motorcycle extends Vehicle {
-  private category: EnumCategory;
+  private category: string;
   private engineCapacity: number;
 
   constructor(newMotorcycle: IMotorycle) {
@@ -13,7 +12,7 @@ export default class Motorcycle extends Vehicle {
   }
 
   public getCategory() { return this.category; }
-  public setCategory(newCategory: EnumCategory) { this.category = newCategory; }
+  public setCategory(newCategory: string) { this.category = newCategory; }
 
   public getEngine() { return this.engineCapacity; }
   public setEngine(newEngine: number) { this.engineCapacity = newEngine; }
