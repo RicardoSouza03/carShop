9941beb0bc27d4223fae00c6b80c755a0ecd99a8
Ricardo_Souza <rl03souza@gmail.com>
2023-02-14 14:14:15 -0300

    Tests: adding mocks for motorcycle creation

---
+++ tests/Mocks/NewMotorcycle.mock.ts
@@ -0,0 +1,25 @@
+const motorcycleInput: IMotorycle = {
+  model: 'Honda Cb 600f Hornet',
+  year: 2005,
+  color: 'Yellow',
+  status: true,
+  buyValue: 30.000,
+  category: 'Street',
+  engineCapacity: 600,
+};
+
+const motorcycleOutPut: Motorcyle = new Motorcyle({
+  id: '6348513f34c397abcad040b2',
+  model: 'Honda Cb 600f Hornet',
+  year: 2005,
+  color: 'Yellow',
+  status: true,
+  buyValue: 30.000,
+  category: 'Street',
+  engineCapacity: 600,
+});
+
+export {
+  motorcycleInput,
+  motorcycleOutPut,
+};
