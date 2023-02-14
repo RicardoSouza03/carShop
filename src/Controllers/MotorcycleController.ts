f1f595e854886731f5d69373c9dedc23c081cf87
Ricardo_Souza <rl03souza@gmail.com>
2023-02-14 15:26:12 -0300

    Feat: adding same methods from car controller into motorcycle controller

--- src/Controllers/MotorcycleController.ts
+++ src/Controllers/MotorcycleController.ts
@@ -25,4 +25,41 @@ export default class MotorcycleController extends AbstractController {
     const createdMotorcycle = await this.service.registerMotorcycle(newMotorcycle);
     return this.res.status(201).json(createdMotorcycle);
   }
+
+  public async getAll(): Promise<Response> {
+    const allMotorcycles = await this.service.getAll();
+    return this.res.status(200).json(allMotorcycles);
+  }
+
+  public async getById() {
+    const { id } = this.req.params;
+
+    try {
+      const motorcycleById = await this.service.getById(id);
+      return this.res.status(200).json(motorcycleById);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+
+  public async updateById() {
+    const { id } = this.req.params;
+
+    const motorcycleToUpdate: IMotorycle = {
+      model: this.req.body.model,
+      color: this.req.body.color,
+      year: this.req.body.year,
+      status: this.req.body.status || false,
+      buyValue: this.req.body.buyValue,
+      category: this.req.body.category,
+      engineCapacity: this.req.body.engineCapacity,
+    };
+
+    try {
+      const updatedMotorcycle = await this.service.updateById(id, motorcycleToUpdate);
+      return this.res.status(200).json(updatedMotorcycle);
+    } catch (error) {
+      this.next(error);
+    }
+  }
 }
