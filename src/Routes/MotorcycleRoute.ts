5f28d4da2ff6d01c52efad65894d2e83e044b120
Ricardo_Souza <rl03souza@gmail.com>
2023-02-14 15:26:33 -0300

    Feat: creating all necessary routes for motorcycle

--- src/Routes/MotorcycleRoute.ts
+++ src/Routes/MotorcycleRoute.ts
@@ -3,9 +3,24 @@ import MotorcycleController from '../Controllers/MotorcycleController';
 
 const motorcycleRoute = Router();
 
+motorcycleRoute.put(
+  '/:id',
+  (req, res, next) => new MotorcycleController(req, res, next).updateById(),
+);
+
 motorcycleRoute.post(
   '/',
   (req, res, next) => new MotorcycleController(req, res, next).create(),
+);
+
+motorcycleRoute.get(
+  '/:id',
+  (req, res, next) => new MotorcycleController(req, res, next).getById(),
+);
+
+motorcycleRoute.get(
+  '/',
+  (req, res, next) => new MotorcycleController(req, res, next).getAll(),
 );
 
 export default motorcycleRoute;
