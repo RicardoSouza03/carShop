4ddfbc0aa17f47d97bc89bbf5ccce6b335d04316
Ricardo_Souza <rl03souza@gmail.com>
2023-02-14 15:25:53 -0300

    Feat: adding same methods from car service into motorcycle service

--- src/Services/MotorcycleService.ts
+++ src/Services/MotorcycleService.ts
@@ -1,9 +1,11 @@
 import Motorcycle from '../Domains/Motorcycle';
 import IMotorycle from '../Interfaces/IMotorcycle';
 import MotorcycleODM from '../Models/Motorcycle';
+import MotorcycleValidate from '../Utils/Validations/MotorcycleValidations';
 
 export default class MotorcycleService {
   private motorcycleODM = new MotorcycleODM();
+  private motorcycleValidations = new MotorcycleValidate();
 
   private createMotorcycle(motorcycle: IMotorycle | null): Motorcycle | null {
     if (motorcycle) {
@@ -15,5 +17,28 @@ export default class MotorcycleService {
   public async registerMotorcycle(motorcycle: IMotorycle) {
     const newMotorcycle = await this.motorcycleODM.create(motorcycle);
     return this.createMotorcycle(newMotorcycle);
+  }
+
+  public async getAll() {
+    const allMotorcycles = await this.motorcycleODM.getAll();
+    return allMotorcycles.map((motorcycle) => this.createMotorcycle(motorcycle));
+  }
+
+  public async getById(id: string) {
+    this.motorcycleValidations.validateMotorcycleId(id);
+    
+    const foundMotorcycle = await this.motorcycleODM.getById(id);
+    this.motorcycleValidations.checkMotorcycleExistence(foundMotorcycle);
+
+    return this.createMotorcycle(foundMotorcycle);
+  }
+
+  public async updateById(id: string, newInfo: IMotorycle) {
+    this.motorcycleValidations.validateMotorcycleId(id);
+
+    await this.motorcycleODM.updateById(id, newInfo);
+    const updatedMotorcycle = await this.getById(id);
+
+    return updatedMotorcycle;
   }
 }
